f31a678bbf1b415bfe9a73b5423d10df246bcb72
chenchianstudent <105021007@live.asia.edu.tw>
2016-09-19 16:05:50 +0800

    hw02  架構
    微架構

--- week01_20160919/src/hw/hw02_105021007.java
+++ week01_20160919/src/hw/hw02_105021007.java
@@ -1,10 +1,15 @@
 package hw;
-
+//105021007 陳麒安
+import java.util.Random;
 public class hw02_105021007 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// 請寫一個程式讓使用者輸入 n 並產生 6 個介於 1~n 之間的亂數 (輸出的亂數可重覆)
+		
+		Random rnd = new Random();
+		
+		
 	}
 
 }
