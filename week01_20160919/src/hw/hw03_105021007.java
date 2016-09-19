35968a178db37c462feee6d82e070f1da4dd37f8
chenchianstudent <105021007@live.asia.edu.tw>
2016-09-19 16:22:38 +0800

    hw0304微調

--- week01_20160919/src/hw/hw03_105021007.java
+++ week01_20160919/src/hw/hw03_105021007.java
@@ -1,10 +1,11 @@
 package hw;
-
+import java.util.Scanner;
 public class hw03_105021007 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//叫糶祘Α琵ㄏノ块 6 计(Τ计)–常彼き块硂 6 计い程
+		Scanner scn=new Scanner(System.in);
 	}
 
 }
--- week01_20160919/src/hw/hw04_105021007.java
+++ week01_20160919/src/hw/hw04_105021007.java
@@ -1,10 +1,14 @@
 package hw;
 
+import java.util.Scanner;
+
 public class hw04_105021007 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//請寫一個程式讓使用者輸入 a 與 b, 程式輸出 a 開 b 次方後的結果，例如 9.0 開 3次方 結果為 2.080083823051904
+		Scanner scn=new Scanner(System.in);
+		
 	}
 
 }
